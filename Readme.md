6d8b921cee6d92d3f2cfff30d66a8611dfa00f48
David Hermoso <juandavidhermoso@gmail.com>
2021-10-30 09:10:48 +0200

    Build site builder

--- Readme.md
+++ Readme.md
@@ -1,9 +1,21 @@
 # Static site builder
-This project is a static site builder
-It's built using just HTML and CSS.
 
-## Steps
+This project is a static site builder It's built using just HTML and CSS.
+Handlebars@4.7.7 is used for parsing HTML, so Handlebars features can be used.
 
-1. npm ci
-2. npm run build:all
-
+## Steps to build a project with ```static-sites-builder```
+1. ```mkdir my-fancy-static-website```
+2. ```cd my-fancy-static-website```
+3. ```npm init --y```
+4. ```n```
+5. Create an ```html``` file you wanna use. Eg: ```index.html```
+6. Create a ```scss``` file.  Eg: ```main.scss```
+7. Create a json file for every language you wanna translate your site into (Eg: src/assets/en.json)
+8. Create an .env file with the following environment props (see .env.example for reference):
+   1. ```html```= Absolute path to your .html file (Eg: src/index.html)
+   2. ```scss```= Absolute path to your .scss file (Eg: src/styles/main.scss)
+   3. ```languages```= List of the languages you wanna translate your site into (Eg: ['es', 'en', 'ca']). For each of them an ```html``` file will be generated.
+   4. ```translations```= Path of the translations file (Eg: src/assets/i18n)
+10. Add the following script to your package.json scripts ```"build:all": "build-site"```
+11. Run ```npm run build```
+12. Check your the ```dist``` directorty ```at my-fancy-static-website``` directory.
---
+++ static-site-builder.js
@@ -0,0 +1,147 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const Handlebars = require('handlebars');
+const buildDateTime = new Date().getTime();
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+const mkdirp = require('mkdirp');
+const minifyHtml = require("@minify-html/js");
+let sourceHTML = fs.readFileSync("src/index.html").toString();
+let sourceSCSS = fs.readFileSync("src/styles/main.scss");
+let dom = new JSDOM(sourceHTML, {runScripts: "dangerously"});
+const sass = require('node-sass');
+const dotenv = require('dotenv');
+const outputDir = 'dist';
+let languages = ['en'];
+let translationsPath = 'src/assets/i18n';
+
+
+build();
+
+function build() {
+  dotenv.config();
+
+  if (!process.env.html) {
+    console.info('*********************');
+    console.info('I need to know which is the HTML source file you wanna use. I am using the default one: "src/index.html"');
+    console.info('\nIn order to let me now another one, I need an environment variable called html with the relative path to it.');
+    console.info('Eg: html=src/map.html');
+    console.info('*********************\n\n');
+  } else {
+    sourceHTML = fs.readFileSync(process.env.html).toString();
+    dom = new JSDOM(sourceHTML, {runScripts: "dangerously"});
+  }
+
+
+  if (!process.env.scss) {
+    console.info('*********************');
+    console.info('I need to know which is the SCSS source file you wanna use. I am using the default one: "src/styles/main.scss"');
+    console.info('\nIn order to let me now another one, I need an environment variable called scss with the relative path to it.');
+    console.info('Eg: scss=src/styles/main.scss');
+    console.info('*********************\n\n');
+  } else {
+    sourceSCSS = fs.readFileSync(process.env.scss);
+  }
+
+  if (!process.env.languages) {
+    console.info('*********************');
+    console.info('I need to know in which languages you want to translate your app');
+    console.info('\nI\'m using English, by default.');
+    console.info('\nIn order to let me now your desired ones, I need an environment variable called languages with a list of languages codes');
+    console.info('Eg: languages=["ca", "es"]');
+    console.info('*********************\n\n');
+  } else {
+    languages = JSON.parse(process.env.languages);
+  }
+
+  if (!process.env.translations) {
+    console.info('*********************');
+    console.info('I need to know where to find translations files');
+    console.info('\nI\'m using \'src/assets/i18n\' by default');
+    console.info('\nIn order to let me now your desired ones, I need an environment variable called languages with a list of languages codes');
+    console.info('Eg: translations=src/assets/my-translations/');
+    console.info('*********************\n\n');
+  } else {
+    translationsPath = JSON.parse(process.env.translations);
+  }
+
+  try {
+    fs.rmSync(outputDir, {recursive: true, force: true});
+
+    mkdirp(outputDir).then(() => {
+      languages.forEach((lang) => {
+        const translations = JSON.parse(fs.readFileSync(`${translationsPath}/${lang}.json`));
+        const withCssLinkTag = addHeadTags(translations);
+
+        fs.writeFileSync(`${outputDir}/${lang}.html`, `<!DOCTYPE html>${getMinifiedHTML(getTranslatedHTML(withCssLinkTag, translations))}`);
+      });
+
+      comeSassToCSS();
+      renameCssFile()
+    });
+  } catch (e) {
+    console.info('****')
+    console.error('Ooops! Something went wrong. Please, check the path of the environment variables you provided.')
+    console.error(e)
+    console.info('****')
+  }
+}
+
+function comeSassToCSS() {
+  const compiledCss = sass.renderSync({
+    file: process.env.scss,
+    outFile: `${outputDir}/styles.css`,
+    outputStyle: 'compressed',
+  }).css;
+
+  fs.writeFileSync(`${outputDir}/styles.css`, compiledCss);
+}
+
+function getTranslatedHTML(file, translations) {
+  const template = Handlebars.compile(file);
+  return template(translations);
+}
+
+function getMinifiedHTML(file) {
+  const cfg = minifyHtml.createConfiguration({keep_closing_tags: true, remove_bangs: false});
+  return minifyHtml.minify(file, cfg);
+}
+
+function addHeadTags(translations) {
+  dom.window.document.head.appendChild(getLinkTag());
+  dom.window.document.head.appendChild(getMetaDescriptionTag(translations));
+  dom.window.document.head.appendChild(getPageTitle(translations));
+
+
+  return dom.window.document.documentElement.outerHTML;
+}
+
+function renameCssFile() {
+  fs.rename(`${outputDir}/styles.css`, `${outputDir}/styles.${buildDateTime}.css`, function (err) {
+    if (err) console.log('ERROR: ' + err);
+  });
+}
+
+function getLinkTag() {
+  const linkEl = dom.window.document.createElement('link');
+  linkEl.setAttribute('rel', 'stylesheet');
+  linkEl.setAttribute('href', `./css/styles.${buildDateTime}.css`);
+
+  return linkEl;
+}
+
+function getMetaDescriptionTag(translations) {
+  const metaDescriptionElement = dom.window.document.createElement('meta');
+  metaDescriptionElement.setAttribute('name', 'description');
+  metaDescriptionElement.setAttribute('content', translations.meta_description);
+
+  return metaDescriptionElement;
+}
+
+function getPageTitle(translations) {
+  const titleElement = dom.window.document.createElement('title');
+  titleElement.text = translations.title;
+
+  return titleElement;
+}
